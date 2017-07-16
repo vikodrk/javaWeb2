a83b6aad23c1741e43463d6ffc8a95cee937ae80
Oscar-Galan <Oscar-Galan@192.168.1.72>
2017-07-16 11:31:59 -0600

    Comit Inicial
    Se sube proyecto

---
+++ Oscar/curso-java/src/main/java/mx/com/cetech/javaweb/cursojava/App.java
@@ -0,0 +1,13 @@
+package mx.com.cetech.javaweb.cursojava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
